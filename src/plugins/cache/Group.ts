ca27d5102755f5c0e0c9205de2b4d42ddd29e2dd
Ayaka <user.satoshi69@gmail.com>
2023-04-05 23:51:44 +0530

    added structures to deps

--- src/plugins/cache/Group.ts
+++ src/plugins/cache/Group.ts
@@ -8,8 +8,8 @@ import channelSweeper from "./sweepers/channelManager.js";
 import emojiSweeper from "./sweepers/emojiManager.js";
 import guildSweeper from "./sweepers/guildManager.js";
 import messageSweeper from "./sweepers/messageManager.js";
-
-export default class Group<K = unknown, V = unknown> extends Map<K, V> {
+import {Group as G} from "@akarui/structures";
+export default class Group<K = unknown, V = unknown> extends G<K, V> {
     config: {
         sweeper: {
             type: SweeperType;
@@ -23,9 +23,9 @@ export default class Group<K = unknown, V = unknown> extends Map<K, V> {
   };
   constructor(
     config: GroupConfigOptions,
-    it?: Iterable<readonly [K, V]> | null | undefined,
+    it?: Iterable<readonly [K, V]> | undefined,
   ) {
-      super( it );
+      super( config.size ?? Infinity, it );
       //@ts-ignore
       this.config = config;
       if ( !this.config.sweeper ) this.config.sweeper = this.defaultSweep();
@@ -65,81 +65,4 @@ export default class Group<K = unknown, V = unknown> extends Map<K, V> {
             }, this.config.sweeper.timeLimit );
     }
 
-    filter ( func: ( val: V, key: K, grp: this ) => boolean )
-    {
-        const res: V[] = [];
-        for ( const [key,value] of this.entries() )
-        {
-            if ( func( value, key, this ) ) res.push( value );
-        }
-        return res;
-    }
-    V ()
-    {
-        return [...this.values()];
-    }
-    K ()
-    {
-        return [...this.keys()];
-    }
-    top ( amount: number = 1)
-    {
-        const res = this.V().slice( 0, amount );
-        if ( res.length === 0 ) return undefined;
-        if ( res.length === 1 ) return res[ 0 ];
-        return res;
-    }
-    bottom ( amount: number = 1)
-    {
-        const res = this.V().slice( -amount );
-        if ( res.length === 0 ) return undefined;
-        if ( res.length === 1 ) return res[ 0 ];
-        return res;
-    }
-    shift ()
-    {
-        const key = this.keys().next().value;
-        if ( !key ) return undefined;
-        const val = this.get( key );
-        this.delete( key );
-        return val;
-    }
-    shiftN ( amount: number )
-    {
-        const res: V[] = [];
-        for ( let i = 0; i < amount; i++ )
-        {
-            const val = this.shift();
-            if ( !val ) break;
-            res.push( val );
-        }
-        if ( res.length === 0 ) return undefined;
-        if ( res.length === 1 ) return res[ 0 ];
-        return res;
-    }
-    pop ()
-    {
-        const key = this.K()[ this.size - 1 ];
-        if ( !key ) return undefined;
-        const val = this.get( key );
-        this.delete( key );
-        return val;
-    }
-    popN ( amount: number )
-    {
-        const res: V[] = [];
-        const keys = this.K();
-        let index = keys.length - 1;
-        for ( let i = 0; i < amount; i++ )
-        {
-            const val = this.get( keys[ index ] );
-            if ( !val ) break;
-            res.push( val );
-            this.delete( keys[ index ] );
-            index--;
-        }
-        if ( res.length === 0 ) return undefined;
-        if ( res.length === 1 ) return res[ 0 ];
-        return res;
-    }
 }
