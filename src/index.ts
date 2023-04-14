1c6eea4352539edbb7591ecba360790b58e80e40
Ayaka <user.satoshi69@gmail.com>
2023-04-15 00:03:34 +0530

    fixed paths

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-export * from "./classes";
-import Client from "./client";
-import request from "./request";
-export * from "./request";
+export * from "./classes/index.js";
+import Client from "./client/index.js";
+import request from "./request/index.js";
+export * from "./request/index.js";
 import QueueManager from "./request/queue.js";
-import EventManager from "./events";
+import EventManager from "./events/index.js";
 import Group from "./plugins/cache/Group.js";
 export * from "./plugins/cache/Cacher.js";
-import createCacheManager from "./plugins/cache";
+import createCacheManager from "./plugins/cache/index.js";
 
 export * from "./typings/interface.js";
 export * from "./typings/types.js";
@@ -17,6 +17,6 @@ export * from "./utils/libconstants.js";
 export * from "./utils/api.js";
 export * from "./utils/constants.js";
 
-import Websocket from "./websocket";
+import Websocket from "./websocket/index.js";
 
 export { Client, request, QueueManager, EventManager, Group, createCacheManager, Websocket }
